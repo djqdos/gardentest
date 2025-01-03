6d249c2dc800d8a8924bb7123c31825a1d47316f
Rich <richard.bartlett@gmail.com>
2025-01-03 14:02:32 +0000

    dfgd

--- Infrastructure/Terraform/1-argocd.tf
+++ Infrastructure/Terraform/1-argocd.tf
@@ -5,7 +5,7 @@ resource "helm_release" "argocd" {
   chart = "argo-cd"
   namespace = "argocd"
   create_namespace = true
-  version = "3.35.4"
+  version = "7.7.12"
 
   values = [file("argocd-values.yml")]
 }
